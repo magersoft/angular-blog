7f795bada355fb981cd0203e0a3d555de5f4cbac
v.mager <v.mager@b2b-center.ru>
2020-01-04 18:10:07 +0300

    enviroment + firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyCQxR4vbog3FHkhL4V1Gakdz-423fRw-ns'
+  apiKey: 'AIzaSyCQxR4vbog3FHkhL4V1Gakdz-423fRw-ns',
+  DbUrl: 'https://angular-blog-27d64.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyCQxR4vbog3FHkhL4V1Gakdz-423fRw-ns'
+  apiKey: 'AIzaSyCQxR4vbog3FHkhL4V1Gakdz-423fRw-ns',
+  DbUrl: 'https://angular-blog-27d64.firebaseio.com'
 };
 
 /*
