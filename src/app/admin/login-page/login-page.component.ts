40330d64fcb3af4e5f787342755533c4d7fc8834
v.mager <v.mager@b2b-center.ru>
2020-01-04 16:41:57 +0300

    authenticated + guard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -10,10 +12,21 @@ import {User} from '../../shared/interfaces';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
+  message: string;
 
-  constructor() { }
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params.loginAgain) {
+        this.message = 'Для начала авторизуйтесь!';
+      }
+    });
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
@@ -31,9 +44,19 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
     };
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
+    });
   }
 }
